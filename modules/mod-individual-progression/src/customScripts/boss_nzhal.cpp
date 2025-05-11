c5aecba702fe7b20c3a0a02c7f48c82cd2719435
Rza1337 <ryan_gillies55@hotmail.com>
2025-05-12 00:31:35 +0100

    Attempt to buff nzhal

--- modules/mod-individual-progression/src/customScripts/boss_nzhal.cpp
+++ modules/mod-individual-progression/src/customScripts/boss_nzhal.cpp
@@ -3,14 +3,32 @@
 
 enum Spells
 {
-    SPELL_SHADOW_BOLT = 9613,
-    SPELL_ENRAGE = 8599
+    SPELL_VERSE_1 = 70011, // Armor debuff (tank killer)
+    SPELL_VERSE_2 = 70012, // Reflect damage (DPS punisher)
+    SPELL_VERSE_3 = 70013, // Healing = damage (healer trap)
+    SPELL_CRESCENDO = 70014, // AoE scaling effect
+    SPELL_CRESCENDO_DOT = 70015, // Lingering DOT
+    SPELL_SUMMON_ECHO = 70016, // Summon Twisted Echo
 };
 
 enum Events
 {
-    EVENT_SHADOW_BOLT = 1,
-    EVENT_ENRAGE = 2
+    EVENT_HEAD_CAST = 1,
+    EVENT_CRESCENDO,
+    EVENT_VERSE_1_TICK,
+};
+
+enum SayNzhal
+{
+    SAY_AGGRO = 0,
+    SAY_HEAD_1,
+    SAY_HEAD_2,
+    SAY_HEAD_3,
+    SAY_INTERRUPTED,
+    SAY_CRESCENDO_0,
+    SAY_CRESCENDO_1,
+    SAY_CRESCENDO_2,
+    SAY_CRESCENDO_3,
 };
 
 class boss_nzhal : public CreatureScript
@@ -23,19 +41,74 @@ public:
         boss_nzhalAI(Creature* creature) : ScriptedAI(creature) { }
 
         EventMap events;
-        bool enraged;
+        uint8 currentHead = 0;
+        uint8 interruptedHeads = 0;
+        bool headCasting = false;
 
         void Reset() override
         {
             events.Reset();
-            enraged = false;
-            events.ScheduleEvent(EVENT_SHADOW_BOLT, 5000);
-            events.ScheduleEvent(EVENT_ENRAGE, 1000);
+            currentHead = 0;
+            interruptedHeads = 0;
+            headCasting = false;
         }
 
-        void JustEngagedWith(Unit* who) override
+        void JustEngagedWith(Unit* /*who*/) override
         {
-            me->CallForHelp(60.0f);
+            Talk(SAY_AGGRO);
+            events.ScheduleEvent(EVENT_HEAD_CAST, 8000);
+        }
+
+        void SpellInterrupted(uint32 spellId, uint32 /*school*/) override
+        {
+            if (headCasting && (spellId == SPELL_VERSE_1 || spellId == SPELL_VERSE_2 || spellId == SPELL_VERSE_3))
+            {
+                interruptedHeads++;
+                headCasting = false;
+                Talk(SAY_INTERRUPTED);
+                // Remove auras if needed
+                for (Player* player : me->GetPlayersInRange(100.0f))
+                {
+                    player->RemoveAura(SPELL_VERSE_2);
+                    player->RemoveAura(SPELL_VERSE_3);
+                }
+            }
+        }
+
+        void DoCrescendo()
+        {
+            switch (interruptedHeads)
+            {
+                case 0:
+                    Talk(SAY_CRESCENDO_0);
+                    DoCastAOE(38628); // Fel Explosion
+                    for (Player* player : me->GetPlayersInRange(100.0f))
+                    {
+                        me->CastSpell(player, 17293, true); // Shadow DOT
+                        float x = player->GetPositionX() + frand(-2.f, 2.f);
+                        float y = player->GetPositionY() + frand(-2.f, 2.f);
+                        float z = player->GetPositionZ();
+                        if (Creature* add = me->SummonCreature(15318, x, y, z, 0.0f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000))
+                            add->Attack(player, true);
+                    }
+                    break;
+                case 1:
+                    Talk(SAY_CRESCENDO_1);
+                    DoCastAOE(38628);
+                    for (Player* player : me->GetPlayersInRange(100.0f))
+                        me->CastSpell(player, 17293, true);
+                    break;
+                case 2:
+                    Talk(SAY_CRESCENDO_2);
+                    DoCastAOE(38628);
+                    break;
+                case 3:
+                    Talk(SAY_CRESCENDO_3);
+                    break;
+            }
+            currentHead = 0;
+            interruptedHeads = 0;
+            events.ScheduleEvent(EVENT_HEAD_CAST, 20000);
         }
 
         void UpdateAI(uint32 diff) override
@@ -50,21 +123,42 @@ public:
 
             switch (events.ExecuteEvent())
             {
-                case EVENT_SHADOW_BOLT:
-                    DoCastVictim(SPELL_SHADOW_BOLT);
-                    events.ScheduleEvent(EVENT_SHADOW_BOLT, 5000);
-                    break;
+                case EVENT_HEAD_CAST:
+                {
+                    headCasting = true;
+                    currentHead++;
+                    switch (currentHead)
+                    {
+                        case 1:
+                            Talk(SAY_HEAD_1);
+                            for (uint8 i = 0; i < 4; ++i)
+                                events.ScheduleEvent(EVENT_VERSE_1_TICK, i * 1000);
+                            break;
+                        case 2:
+                            Talk(SAY_HEAD_2);
+                            for (Player* player : me->GetPlayersInRange(100.0f))
+                                player->AddAura(33711, player); // Spell Reflection
+                            break;
+                        case 3:
+                            Talk(SAY_HEAD_3);
+                            for (Player* player : me->GetPlayersInRange(100.0f))
+                                player->AddAura(28194, player); // Corrupted Healing
+                            break;
+                    }
 
-                case EVENT_ENRAGE:
-                    if (!enraged && me->HealthBelowPct(30))
-                    {
-                        DoCast(me, SPELL_ENRAGE);
-                        enraged = true;
-                    }
+                    if (currentHead < 3)
+                        events.ScheduleEvent(EVENT_HEAD_CAST, 10000);
                     else
-                    {
-                        events.ScheduleEvent(EVENT_ENRAGE, 1000);
-                    }
+                        events.ScheduleEvent(EVENT_CRESCENDO, 10000);
+                    break;
+                }
+                case EVENT_CRESCENDO:
+                    headCasting = false;
+                    DoCrescendo();
+                    break;
+                case EVENT_VERSE_1_TICK:
+                    if (Unit* tank = me->GetVictim())
+                        tank->AddAura(27177, tank); // Sunder Armor
                     break;
             }
 
